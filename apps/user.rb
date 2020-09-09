18a27125177972ec63c89c6d0b069f21893373cf
nick_baccete <nickbaccete@gmail.com>
2020-09-08 21:36:12 -0400

    require relatives

---
+++ apps/user.rb
@@ -0,0 +1,2 @@
+require_relative '../app/drinks.rb'
+require_relative '../app/usersdrinks.rb'
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
-
-
+require_relative '../app/drinks.rb'
+require_relative '../app/user.rb'
+require_relative '../app/usersdrinks.rb'
 
 puts "HELLO WORLD"
