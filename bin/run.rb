ad064e4e12d1c2fd9aa4a9f4a6f8ffff7bb2a52c
nick_baccete <nickbaccete@gmail.com>
2020-09-08 22:01:19 -0400

    maybe?

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,8 @@
 require_relative '../config/environment'
+require_relative '../apps/drinks.rb'
+require_relative '../apps/user.rb'
+require_relative '../apps.userdrinks.rb'
+
 
 
 puts "HELLO WORLD"
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 require 'bundler'
-require_relative '../bin/apps'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
