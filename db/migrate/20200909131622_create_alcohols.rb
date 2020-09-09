eb0273408d4bd6e12798ac7aba8f9e87508a4011
theprivatepark <anneypark@gmail.com>
2020-09-09 10:52:09 -0400

    migrate files updates with tables

--- db/migrate/20200909131622_create_alcohols.rb
+++ db/migrate/20200909131622_create_alcohols.rb
@@ -1,4 +1,9 @@
 class CreateAlcohols < ActiveRecord::Migration[6.0]
   def change
+    create_table :alcohols do |t|
+      t.string :name
+      t.string :type
+      t.integer :alcohol_percentage
+    end
   end
 end
--- db/migrate/20200909131642_create_useralcohols.rb
+++ db/migrate/20200909131642_create_useralcohols.rb
@@ -1,4 +1,9 @@
 class CreateUseralcohols < ActiveRecord::Migration[6.0]
   def change
+    create_table :useralcohols do |t|
+      t.integer :user_id
+      t.integer :drink_id
+      t.integer :rating
+    end
   end
 end
