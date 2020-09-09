be823f6e6985552679c54ef89fe132f2ce8c0891
theprivatepark <anneypark@gmail.com>
2020-09-08 22:56:35 -0400

    class alcohol initialize + all complete

---
+++ apps/alcohol.rb
@@ -0,0 +1,18 @@
+class Alcohol
+
+    attr_reader :name, :type, :alcohol_percentage
+
+    @@all = []
+
+    def initialize(name, type, alcohol_percentage)
+        @name = name
+        @type = type
+        @alcohol_percentage = alcohol_percentage
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+    
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,10 @@
 require_relative '../config/environment'
-require_relative '../apps/drinks.rb'
+require_relative '../apps/alcohol.rb'
 require_relative '../apps/user.rb'
 require_relative '../apps/userdrinks.rb'
 
 
 
 puts "HELLO WORLD"
+
+binding.pry
