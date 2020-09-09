e21f5d269f8a87911df2e9aca4bc7a5bbe227e52
nick_baccete <nickbaccete@gmail.com>
2020-09-08 22:09:48 -0400

    finally

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment'
 require_relative '../apps/drinks.rb'
 require_relative '../apps/user.rb'
-require_relative '../apps.userdrinks.rb'
+require_relative '../apps/userdrinks.rb'
 
 
 
