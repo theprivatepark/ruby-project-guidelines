9615d5c05b54405c10f8f6e439e93e80f75192e1
theprivatepark <anneypark@gmail.com>
2020-09-09 17:35:23 -0400

    added CLI into run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -4,7 +4,46 @@ require_relative '../apps/user.rb'
 require_relative '../apps/useralcohol.rb'
 
 
+def welcome 
+puts "Please input your username."
+user_input = gets.chomp
+puts "#{user_input}, welcome to Alcoholics Not Anonymous!"
+end
 
-puts "HELLO WORLD"
+def current_list?
+  puts "Do you want to see your current drink list? (Yes/No)" 
+  user_input = gets.chomp
+  if user_input == "Yes" 
+    puts "FUCKING LIST"
+  else
+    puts "FUCK YOU TOO"
+  end
+end
 
-binding.pry
+def favorites_list
+  puts "Do you want to see your favorites list? (Yes/No)"
+  user_input = gets.chomp
+  if user_input == "Yes"
+    puts "HERE'S YOUR FUCKING FAV LIST"
+  else
+    puts "FUCK YOU AGAIN"
+  end
+end
+
+def wish_list
+  puts "Do you want to see your wish list? (Yes/No)"
+  user_input = gets.chomp
+  if user_input == "Yes"
+    puts "HERE'S YOUR WISH LIST BITCH"
+  else
+    puts "FUCK U 2"
+  end
+end
+
+welcome
+current_list?
+favorites_list
+wish_list
+
+
+# binding.pry
